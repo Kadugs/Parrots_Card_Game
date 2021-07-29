0898600b909c8fd6707d209fc668bf89326571f2
Kadugs <cadu20.gomes@gmail.com>
2021-07-29 11:42:57 -0300

    implementada seleção de cartas iguais

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let numCartas = 0;
 const baralho = document.querySelector(".baralho");
 const cards = [];
-let i;
-const parrotCards = [`<img src="/src/bobrossparrot.gif" class="escondido simbolo-carta">`,
- `<img src="/src/explodyparrot.gif" class="escondido simbolo-carta">`, `<img src="/src/fiestaparrot.gif" class="escondido simbolo-carta">`,
-`<img src="/src/metalparrot.gif" class="escondido simbolo-carta">`,`<img src="/src/revertitparrot.gif" class="escondido simbolo-carta">`,
-`<img src="/src/tripletsparrot.gif" class="escondido simbolo-carta">`,`<img src="/src/unicornparrot.gif" class="escondido simbolo-carta">`]
+const tiposCartas = [];
+let contador = 0;
+const parrotCards = [`bobrossparrot`, `explodyparrot`, `fiestaparrot`,
+`metalparrot`,`revertitparrot`, `tripletsparrot`,`unicornparrot`];
     
 while(numCartas < 4 || numCartas > 14 || numCartas % 2 !== 0 || !numCartas) {
     // numCartas = prompt("Quantas cartas quer jogar?");
@@ -13,19 +12,33 @@ while(numCartas < 4 || numCartas > 14 || numCartas % 2 !== 0 || !numCartas) {
     // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA LEMBRAR DE ALTERAR ISSO PELO AMOR DE DEUS 
     // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 }
-for(i = 0; i < numCartas / 2; i++) {
+for(let i = 0; i < numCartas / 2; i++) {
     for(let j = 0; j < 2; j++) {
-        cards.push(`<div class="card" onclick="vira(this)"> <img src="src/front.png" class="front-parrot"> ${parrotCards[i]}</div>`);
+        cards.push(`<div class="${parrotCards[i]} card" onclick="vira(this);"> <img src="src/front.png" class="front-parrot"> <img src="/src/${parrotCards[i]}.gif" class="simbolo-carta"></div>`);
+        
     }
 }
 cards.sort(randomizer);
-for(i = 0; i < numCartas; i++) {
+for(let i = 0; i < numCartas; i++) {
     baralho.innerHTML += cards[i];
     console.log(cards.length)
 }
 function randomizer() {
     return Math.random() -0.5 ;
 }
-function vira(carta) {
-    carta.classList.add("card-clicado");     
+let cartaClicada;
+function vira(elemento) {
+    elemento.classList.add("card-clicado");
+    if(!cartaClicada) {
+        cartaClicada = elemento;
+    } else if (elemento.classList.item(0) === cartaClicada.classList.item(0)) {
+        cartaClicada.classList.add("acertou");
+        elemento.classList.add("acertou");
+        elemento.classList.remove("card-clicado");
+        cartaClicada = undefined;
+    } else {
+        elemento.classList.add("errou");
+        elemento.classList.remove("errou");
+        elemento.classList.remove("card-clicado");
+    }
 }
